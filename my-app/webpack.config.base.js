039c3861ee3ca25ec3ac7274d36c64c927d4f5e6
dujiaxing <dujiaxing@douyu.tv>
2018-04-05 10:07:18 +0800

    gengagi

--- my-app/webpack.config.base.js
+++ my-app/webpack.config.base.js
@@ -1,7 +1,10 @@
-
+const path =require("path");
 module.exports={
   entry:["./src/index"],
   output:{
-    path:path.join(__dirname,"build")
-  }
+    path:path.join(__dirname,"build"),
+    filename:"bundle.js"
+  },
+  resolve:{extensions:[".js"]}
+
 }
