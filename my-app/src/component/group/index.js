ce83d13804fc685a7015a775f67e8d3c3e599400
dujiaxing <dujiaxing@douyu.tv>
2018-04-17 20:48:36 +0800

    genggai

--- my-app/src/component/group/index.js
+++ my-app/src/component/group/index.js
@@ -18,6 +18,7 @@ export default class Group1 extends React.Component {
     return (
       <div>
         <p>group1!!!</p>
+        <video src="http://221.228.226.15/15/p/r/u/z/pruzntjdzdmffkmpoifamfhjasigrv/sh.yinyuetai.com/F37E0162A9D44ABE9ECEC90895C5B9E3.mp4" allowFullScreen="true" quality="high" width="480" height="350" align="middle" allowScriptAccess="always" type="application/x-shockwave-flash"></video>
       </div>
     );
   }
